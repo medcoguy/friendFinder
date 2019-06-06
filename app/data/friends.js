3bd6a76a6587849e71569047756d6df01e19d298
medcoguy <46691562+medcoguy@users.noreply.github.com>
2019-06-05 21:47:03 -0500

    Initial commit

---
+++ app/data/friends.js
@@ -0,0 +1,120 @@
+// Array of friends to match with user's answers
+var friends = [{
+    "name": "Arnold",
+    "photo": 'https://cdn3.vectorstock.com/i/1000x1000/56/92/angry-colorful-dog-head-on-pop-art-style-vector-23265692.jpg',
+    "scores": [
+
+    //     name: "Arnold",
+    // photo: 'https://cdn3.vectorstock.com/i/1000x1000/56/92/angry-colorful-dog-head-on-pop-art-style-vector-23265692.jpg',
+    // scores: [
+       " 5",
+      "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+       "1"
+    ]
+},
+{
+    "name": "Buster",
+    "photo": 'https://ssl.bigstockimages.com/2/8/2/large2/282323179.jpg',
+    "scores": [
+        2,
+        4,
+        5,
+        2,
+        3,
+        1,
+        3,
+        5,
+        1,
+        4
+    ]
+},
+{
+    "name": "Toby",
+    "photo": 'https://img.freepik.com/free-vector/dog-colorful-pop-art-design_25750-2.jpg?size=338&ext=jpg',
+    "scores": [
+        5,
+        5,
+        5,
+        1,
+        1,
+        1,
+        3,
+        2,
+        3,
+        2
+    ]
+},
+{
+    "name": "Scout",
+    "photo": 'https://t4.ftcdn.net/jpg/02/13/74/55/500_F_213745511_gvRG8vMB9TO0QVPxNHICew693ImoynKV.jpg',
+    "scores": [
+        2,
+        2,
+        2,
+        3,
+        4,
+        3,
+        4,
+        5,
+        1,
+        1
+    ]
+},
+{
+    "name": "Loulou",
+    "photo": 'https://cdn4.vectorstock.com/i/1000x1000/54/63/pop-art-japanese-shiba-inu-dog-with-food-bone-vector-18885463.jpg',
+    "scores": [
+        1,
+        1,
+        4,
+        4,
+        2,
+        2,
+        4,
+        3,
+        3,
+        3
+    ]
+},
+{
+    "name": "Annie",
+    "photo": 'https://previews.123rf.com/images/gouache7/gouache71706/gouache7170600092/81168113-dog-pop-art-on-a-pink-background-with-stars-vector-illustration.jpg',
+    "scores": [
+        1,
+        1,
+        1,
+        5,
+        5,
+        5,
+        3,
+        3,
+        2,
+        2
+    ]
+},
+{
+    "name": "Dumbo",
+    "photo": 'https://cdn1.vectorstock.com/i/1000x1000/58/50/pop-art-springer-spaniel-cute-dirty-dog-vector-20485850.jpg',
+    "scores": [
+        5,
+        5,
+        5,
+        5,
+        5,
+        1,
+        1,
+        1,
+        3,
+        2
+    ]
+}
+];
+
+module.exports = friends;
